a5546da3126803bc62f32710d5ff52c1ce926c73
Nathan Kau <nathankau@gmail.com>
2016-02-18 11:22:06 -0800

    manually created csv file of station ids, lat,lat, coordinates in landsat image. wrote function to parse into dictionary. get_data fully working but untested!

--- data_formatter/landsat_get_data.py
+++ data_formatter/landsat_get_data.py
@@ -1,11 +1,23 @@
-import csv
-import numpy as np
-import cv2
-import os
 import glob
-from get_data import get_polaris_list
 from datetime import datetime
+import cv2
+import numpy as np
+import csv
+import pandas as pd
+import time
 
+from get_data import get_polaris_list
+
+def load_station_image_lat_long(path='/Users/Nathan/Desktop/Turbidity/SedimentLearning/data_formatter/station_img_coordinates.csv'):
+    df = pd.read_csv(path)
+
+    latlong_coors = zip(df['Lat'],df['Long'])
+    latlong = dict(zip(df['Station'],latlong_coors))
+
+    yx_coors = zip(df['Y'],df['X'])
+    yx = dict(zip(df['Station'],yx_coors))
+
+    return latlong,yx
 
 def get_scene_folders(path='/Users/Nathan/Dropbox/SedimentLearning/data/landsat/'):
     return glob.glob(path + '*/')  # full path of each folder asdfasdf/asdf/asd/fa/LC1209310/
@@ -41,9 +53,11 @@ def get_cloud(all_imgs):
     assert cloud is not None
     return cloud
 
+
 def get_metadata_path(scene_folder):
-    mtl = glob.glob(scene_folder+'*MTL.txt')[0]
+    mtl = glob.glob(scene_folder + '*MTL.txt')[0]
     return mtl
+
 
 def get_datetime_from_metadata(metadata_path):
     date_string = ''
@@ -55,47 +69,49 @@ def get_datetime_from_metadata(metadata_path):
             index = line.find(date_acq)
             if index is not -1:
                 # get the substring from the space after = sign to the end of the line
-                date_string = line[len(date_acq)+index:].strip('\n')
+                date_string = line[len(date_acq) + index:].strip('\n')
 
             center_time = 'SCENE_CENTER_TIME = '
             index = line.find(center_time)
             if index is not -1:
                 # get the substring from the space after = sign to the decimal in the seconds
-                time_string = line[len(center_time)+index:line.find('.')].strip('\"\n')
+                time_string = line[len(center_time) + index:line.find('.')].strip('\"\n')
 
             # both date and time have been found then quit
             if date_string is not '' and time_string is not '':
                 break
 
         # parse the datetime object
-        date_time_string = date_string +' ' + time_string
+        date_time_string = date_string + ' ' + time_string
         dt = datetime.strptime(date_time_string, "%Y-%m-%d %H:%M:%S")
         return dt
 
-def get_data(station_locs_dict, station_image_coors_dict):
-    data_columns = ['date_time', 'cf_mask_quality', 'cloud','station_ID', 'lat', 'long', 'landsat_scene','reflec_1', 'reflec_2', 'reflec_3',
-                    'reflec_4', 'reflec_5', 'reflec_6',
-                    'reflec_7']
+
+def get_data((station_locs_dict, station_image_coors_dict)):
+    data_columns = ['date_time', 'cf_mask_quality', 'cloud', 'station_ID', 'lat', 'long', 'landsat_scene', 'reflec_1',
+                    'reflec_2', 'reflec_3','reflec_4', 'reflec_5', 'reflec_6','reflec_7']
+
 
     data = {}
     for col in data_columns:
         data[col] = np.array([])  # initialize
 
-    folders = get_scene_folders('/Users/Nathan/Dropbox/SedimentLearning/data/landsat/')
+    landsat_data_path = '/Users/Nathan/Dropbox/SedimentLearning/data/landsat/'
+    folders = get_scene_folders(landsat_data_path)
 
-    for scene_folder in folders: # code to execute for each scene
+    for scene_folder in folders:  # code to execute for each scene
         # get names of all images in the scene folder
         imgs = get_scene_imgs(scene_folder)
 
         # get the paths for the different types of image
         sr_paths = get_sr_band_imgs(imgs)
-        cf_path= get_cfmask(imgs)
+        cf_path = get_cfmask(imgs)
         cloud_path = get_cloud(imgs)
 
         # get the name of the scene extracted from cf path
-        scene_name = cf_path[:cf_path.find('_')]
+        scene_name = cf_path[cf_path.rfind('/')+1:cf_path.rfind('_')]
 
-        data['landsat_scene'] = np.append(data['landsat_scene'],[scene_name]*len(station_image_coors_dict.keys()))
+        data['landsat_scene'] = np.append(data['landsat_scene'], [scene_name] * len(station_image_coors_dict.keys()))
         data['station_ID'] = np.append(data['station_ID'], station_image_coors_dict.keys())
 
         # write station locations
@@ -106,7 +122,7 @@ def get_data(station_locs_dict, station_image_coors_dict):
 
         # write time
         datetime = str(get_datetime_from_metadata(get_metadata_path(scene_folder)))
-        data['date_time'] = np.append(data['date_time'],[datetime] * len(station_image_coors_dict.keys()))
+        data['date_time'] = np.append(data['date_time'], [datetime] * len(station_image_coors_dict.keys()))
 
         # write cloud mask into dictionary
         cf_img = cv2.imread(cf_path, cv2.IMREAD_ANYDEPTH)
@@ -120,11 +136,10 @@ def get_data(station_locs_dict, station_image_coors_dict):
             cloud = cloud_img[tuple(station_image_coors_dict[ID])]  # need the tuple cast if station returns an array
             data['cloud'] = np.append(data['cloud'], cloud)
 
-
         # initialize list of unwritten bands with all
         unwritten_bands = ['reflec_1', 'reflec_2', 'reflec_3',
-                    'reflec_4', 'reflec_5', 'reflec_6',
-                    'reflec_7']
+                           'reflec_4', 'reflec_5', 'reflec_6',
+                           'reflec_7']
 
         # write reflectance images into dictionary
         for sr_file in sr_paths:  # must record which band it is somewhere
@@ -136,32 +151,38 @@ def get_data(station_locs_dict, station_image_coors_dict):
 
             # put the reflectance into the dict, append it to the end
             for ID in station_image_coors_dict.keys():
-                reflectance = sr[tuple(station_image_coors_dict[
-                                           ID])]  # raw point value, should average? need the tuple cast if station returns array
+                # raw point value, should average? need the tuple cast if station returns array
+                reflectance = sr[tuple(station_image_coors_dict[ID])]
                 data[band_key] = np.append(data[band_key], reflectance)
 
+        # put np.nan
         for band_key in unwritten_bands:
             for ID in station_image_coors_dict.keys():
-                reflectance = np.nan # ERROR VALUE
+                reflectance = np.nan  # ERROR VALUE
                 data[band_key] = np.append(data[band_key], reflectance)
 
     # check that there are as many data points as stations
     for col in data_columns:
-        print col, len(data[col]), len(folders)*len(station_image_coors_dict.keys())
-        print data[col]
-        assert len(data[col]) == len(folders)*len(station_image_coors_dict.keys())
+        #print col, len(data[col]), len(folders) * len(station_image_coors_dict.keys())
+        #print data[col]
+        assert len(data[col]) == len(folders) * len(station_image_coors_dict.keys())
 
     return data
 
 if __name__ == '__main__':
-    # print get_polaris_list('/Users/Nathan/Dropbox/SedimentLearning/data/polaris/polaris_locations.csv')
 
-    print get_data( {'1': [4, 5], '2': [6, 7]}, {'1': [600, 600], '2': [700, 700]})
+    #print get_polaris_list('/Users/Nathan/Dropbox/SedimentLearning/data/polaris/polaris_locations.csv')
+    before = time.time()
+    dict = load_station_image_lat_long()
+    data =  get_data(dict)
+    print time.time()-before
 
+    data_columns = ['date_time', 'cf_mask_quality', 'cloud', 'station_ID', 'lat', 'long', 'landsat_scene', 'reflec_1',
+                    'reflec_2', 'reflec_3','reflec_4', 'reflec_5', 'reflec_6','reflec_7']
+    before = time.time()
+    df = pd.DataFrame.from_dict(data)
+    print time.time() - before
 
-    '''
-    print get_data_at_station_locations(0, [
-        '/Users/Nathan/Dropbox/SedimentLearning/data/landsat/LC80440342013314-SC20160208223831/LC80440342013314LGN00_sr_band1.tif'], \
-                                        '/Users/Nathan/Dropbox/SedimentLearning/data/landsat/LC80440342013314-SC20160208223831/LC80440342013314LGN00_cfmask.tif', \
-                                        {'1': [4, 5], '2': [6, 7]}, {'1': [600, 600], '2': [700, 700]})
-    '''
+    before = time.time()
+    df.to_csv('/Users/Nathan/Desktop/landsat_data.csv',mode='wb+',index=False)
+    print time.time()-before
--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,83 +1,92 @@
-import csv
-import numpy as np
-import test_lin_reg
 from os import listdir
+
 from os.path import isfile, join
+
 import cv2
+import numpy as np
+
+import test_lin_reg
+
+
 def get_station_locations(path):
     filenames = [path + f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.csv')]
-    #print filenames
+    # print filenames
 
-    X,y= test_lin_reg.get_data(filenames,['insitu_lat','insitu_long'],[])
-    #print X
+    X, y = test_lin_reg.get_data(filenames, ['insitu_lat', 'insitu_long'], [])
+    # print X
     unique_x = np.vstack({tuple(row) for row in X})
 
     print unique_x.shape
     print unique_x
 
-    min_lat,max_lat = min(unique_x[:,0]),max(unique_x[:,0])
-    min_long,max_long = min(unique_x[:,1]),max(unique_x[:,1])
+    min_lat, max_lat = min(unique_x[:, 0]), max(unique_x[:, 0])
+    min_long, max_long = min(unique_x[:, 1]), max(unique_x[:, 1])
 
     print 'Bottom right ' + str((min_lat, max_long))
     print 'Top left ' + str((max_lat, min_long))
 
+
 def get_scenenames_from_metadata(path):
-    filenames = [path + f for f in listdir(path) if isfile(join(path,f)) and f.endswith('.csv')]
-    #print filenames
-    X,y = test_lin_reg.get_data(filenames,['Landsat Scene Identifier'],[])
+    filenames = [path + f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.csv')]
+    # print filenames
+    X, y = test_lin_reg.get_data(filenames, ['Landsat Scene Identifier'], [])
     print 'Shape of scene names {}'.format(X.shape)
 
-    with open('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/scenes.txt','w+b') as f:
-        f.write('\n'.join(X[:,0]))
+    with open('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/scenes.txt', 'w+b') as f:
+        f.write('\n'.join(X[:, 0]))
         print 'Done writing scene names'
 
-def get_AOI(img_path,(img_lat0,img_long0),(img_lat1,img_long1),(aoi_lat0,aoi_long0),(aoi_lat1,aoi_long1)=(None,None),radius = None):
+
+def get_AOI(img_path, (img_lat0, img_long0), (img_lat1, img_long1), (aoi_lat0, aoi_long0),
+            (aoi_lat1, aoi_long1)=(None, None), radius=None):
     # 0 is the top left, 1 is the bottom right
     # for some reason it's usually read lat,long not long, lat
-    #img = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
 
-    img = cv2.imread(img_path,cv2.IMREAD_ANYDEPTH)
+    img = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
     if img is None:
         raise IOError
     print 'Processed image'
 
-    img_height,img_width = img.shape # like a matrix so height first
+    img_height, img_width = img.shape  # like a matrix so height first
 
     # return a square of pixels around the area of interest (aoi) with a square size = 2*radius
     if aoi_lat1 is None:
         # interpolate the image coordinates
-        [img_aoi_x] = np.int32(np.interp([aoi_long0],[img_long0,img_long1],[0,img_width]))
-        [img_aoi_y] = np.int32(np.interp([aoi_lat0],[img_lat1,img_lat0],[img_height,0]))
+        [img_aoi_x] = np.int32(np.interp([aoi_long0], [img_long0, img_long1], [0, img_width]))
+        [img_aoi_y] = np.int32(np.interp([aoi_lat0], [img_lat1, img_lat0], [img_height, 0]))
         if img_lat1 > img_lat0:
-            raise ValueError # interp doesn't work in this case
+            raise ValueError  # interp doesn't work in this case
 
-        print 'AOI point: {}'.format((img_aoi_x,img_aoi_y))
+        print 'AOI point: {}'.format((img_aoi_x, img_aoi_y))
 
         # a square of pixels
         if radius > 0:
-             # annoying min max so image isn't out of bounds
-            return img[max(img_aoi_y-radius,0):min(img_aoi_y+radius,img_height),
-                       max(img_aoi_x-radius,0):min(img_aoi_x+radius,img_width)]
+            # annoying min max so image isn't out of bounds
+            return img[max(img_aoi_y - radius, 0):min(img_aoi_y + radius, img_height),
+                   max(img_aoi_x - radius, 0):min(img_aoi_x + radius, img_width)]
         # just one pixel
         elif radius == 0:
             # put the sr value into an array so i can display it with imshow
-            return np.array([[img[img_aoi_y,img_aoi_x]]])
-        raise ValueError # radius shouldn't be None (you have to set it)
+            return np.array([[img[img_aoi_y, img_aoi_x]]])
+        raise ValueError  # radius shouldn't be None (you have to set it)
 
     # return the specified area in the image
     else:
         # interpolate the image coordinates
-        img_aoi_left,img_aoi_right = np.int32(np.interp([aoi_long0,aoi_long1],[img_long0,img_long1],[0,img_width]))
-        img_aoi_top,img_aoi_bottom = np.int32(np.interp([aoi_lat0,aoi_lat1],[img_lat1,img_lat0],[img_height,0]))
-        #print 'aoi image left right: {},{}'.format(img_aoi_left,img_aoi_right)
-        return img[img_aoi_top:img_aoi_bottom,img_aoi_left:img_aoi_right]
+        img_aoi_left, img_aoi_right = np.int32(
+            np.interp([aoi_long0, aoi_long1], [img_long0, img_long1], [0, img_width]))
+        img_aoi_top, img_aoi_bottom = np.int32(np.interp([aoi_lat0, aoi_lat1], [img_lat1, img_lat0], [img_height, 0]))
+        # print 'aoi image left right: {},{}'.format(img_aoi_left,img_aoi_right)
+        return img[img_aoi_top:img_aoi_bottom, img_aoi_left:img_aoi_right]
+
 
 if __name__ == '__main__':
     data_path = '/Users/Nathan/Dropbox/SedimentLearning/data/full/'
     get_station_locations(data_path)
 
     meta_path = '/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/all_scenes/'
-    #get_scenenames_from_metadata(meta_path)
+    # get_scenenames_from_metadata(meta_path)
 
     ''' META DATA
     <lpgs_metadata_file>LC80440342013298LGN00_MTL.txt</lpgs_metadata_file>
@@ -93,17 +102,18 @@ if __name__ == '__main__':
         '''
     ''' bay location: 37.462640, -122.049717
     tip of sf 37.811068, -122.477161'''
-    #img = get_AOI('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/LC80440342013298LGN00_band11.tif',(38.528160,-123.415310),(36.397470,-120.841200),(37.462640, -122.049717),radius=300)
-    img = get_AOI('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/LC80440342013298LGN00_sr_evi.tif',(38.529033,-123.415482),(36.397332,-120.779472),(37.811068, -122.477161),radius=100)
+    # img = get_AOI('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/LC80440342013298LGN00_band11.tif',(38.528160,-123.415310),(36.397470,-120.841200),(37.462640, -122.049717),radius=300)
+    img = get_AOI('/Users/Nathan/Desktop/Turbidity/SedimentLearning/tests/LC80440342013298LGN00_sr_evi.tif',
+                  (38.529033, -123.415482), (36.397332, -120.779472), (37.811068, -122.477161), radius=100)
 
-    #draw crosshairs
-    h,w = img.shape
-    cv2.line(img,(w/2,0),(w/2,h),(255,255,255))
-    cv2.line(img,(0,h/2),(w,h/2),(255,255,255))
+    # draw crosshairs
+    h, w = img.shape
+    cv2.line(img, (w / 2, 0), (w / 2, h), (255, 255, 255))
+    cv2.line(img, (0, h / 2), (w, h / 2), (255, 255, 255))
 
     print img
     print img.shape
-    cv2.imshow('hello',img)
+    cv2.imshow('hello', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
